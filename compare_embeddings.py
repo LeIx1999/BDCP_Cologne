2875a262fe6d890f893d8ebc26370c1980df9389
Philipp Boden <philipp.boden@fom-net.de>
2023-06-27 20:40:03 +0200

    added the comparison of an input and the degrees and also an input and the jobs (small problem left)

--- compare_embeddings.py
+++ compare_embeddings.py
@@ -20,6 +20,11 @@ data = pd.read_parquet("joblist_full.parquet")
 # filter out duplicates
 data = data.drop_duplicates(subset=["title_txt"])
 
+#Wieso mehr embeddings als Jobs?
+#max_job_data = len(data) #151028
+#max_embedding = len(job_embeddings) #347478
+
+
 #create set of unique degrees
 unique_degrees = set([list(element.keys())[0].split('_')[0] for element in module_embeddings_dicts])
 
@@ -44,7 +49,7 @@ for degree in unique_degrees:
 
 
 
-def get_degrees_for_job(job_embedding: np.array, degree_embeddings: list, n_degrees:int = 5) -> pd.DataFrame:
+def get_degrees_for_job(job_embedding: np.array, degree_embeddings: list, n_degrees:int = 5) -> list:
     #calculate similarity of two vectors
     degree_embeddings_values = [list(element.values())[0] for element in degree_embeddings]
     distance = util.dot_score(degree_embeddings_values, job_embedding).tolist()
@@ -67,11 +72,44 @@ result_jobs_degrees_comparison = []
 for i in range(len(job_embeddings[:9])):
     result_jobs_degrees_comparison.append({data.iloc[i,1]: get_degrees_for_job(job_embeddings[i], mean_degree_embeddings_list)})
 
+
 def create_embeddings_from_input(input: list):
     input_as_string = ""
     for element in input:
         input_as_string = input_as_string + " " + element
     input_embedding = model.encode(input_as_string)
+    return input_embedding
+
+# def get_jobs_for_input(input_embedding: np.array, job_embedding: np.array,  n_jobs:int = 5) -> list:
+#     #calculate distance of two vectors
+#     distance = util.dot_score(input_embedding, job_embedding).tolist()
+#     distance =distance[0]
+#     distance_sorted =distance.copy()
+#     distance_sorted.sort(reverse=True)
+
+#     # get index of best fit
+#     ind_list = [distance.index(element) for element in distance_sorted[:n_jobs]]
+#     most_similar_jobs = [data.iloc[index,1] for index in ind_list]
+#     jobs_with_distance = list(zip(most_similar_jobs, distance_sorted[:n_jobs]))
+
+#     return jobs_with_distance
+
+def get_degrees_for_input(input_embedding: np.array, degree_embeddings: list, n_degrees:int = 5) -> list:
+    #calculate similarity of two vectors
+    degree_embeddings_values = [list(element.values())[0] for element in degree_embeddings]
+    distance = util.dot_score(degree_embeddings_values, input_embedding).tolist()
+    distance = [element[0] for element in distance]
+    distance_sorted = distance.copy()
+    distance_sorted.sort(reverse=True)
+
+    # get index of best fit
+    ind_list = [distance.index(element) for element in distance_sorted[:n_degrees]]
+    most_similar_degrees = [list(degree_embeddings[index].keys())[0] for index in ind_list]
+    degrees_with_distance = list(zip(most_similar_degrees, distance_sorted[:n_degrees]))
+
+    return degrees_with_distance
 
 if len(sys.argv) > 1:
-    input_embeddings = create_embeddings_from_input(sys.argv[1:])
+    input_embedding = create_embeddings_from_input(sys.argv[1:])
+    #most_similar_jobs = get_jobs_for_input(input_embedding, job_embeddings)
+    most_similar_degrees = get_degrees_for_input(input_embedding, mean_degree_embeddings_list)
